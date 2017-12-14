689eedbc224ce0ab2887b08d2a9c8041c32adb8a
Anton Ledström <anton.ledstrom@digitalent.se>
2017-12-14 10:07:35 +0100

    added babel, now server renders

--- server/main.js
+++ server/main.js
@@ -2,11 +2,25 @@ var express = require('express');
 
 var app = new express();
 var parser = require('body-parser');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 
+require('babel-register');
 require('./database.js');
 
+var GroceryItem = require('./models/GroceryItem.js')
+
 app.get('/',(req,res) => {
-    res.render('./../app/index.ejs',{});
+    // res.render('./../app/index.ejs',{});
+    let application = React.createFactory(require('./../app/components/GroceryItemList.jsx'))
+
+    GroceryItem.find((err, doc) => {
+      let generated = ReactDOMServer.renderToString(application({
+        items: doc
+      }));
+
+      res.render('./../app/index.ejs', { reactOutput: generated });
+    })
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
